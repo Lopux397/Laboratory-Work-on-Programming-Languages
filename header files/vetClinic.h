3c27cc9f01e871a8b5979a540b19e40c8bcfbfda
Lopux397 <g1g2g3qq2@gmail.com>
2026-09-16 21:29:02 +0300

    Исправление после Sonar
    Добавил:
    vector
    string_view
    Изменил:
    push_back -> emplace_back;
    сырой цикл for на Range-based for-loop;

--- header files/vetClinic.h
+++ header files/vetClinic.h
@@ -5,31 +5,27 @@
 #include "appeals.h"
 
 #include <iostream>
+#include <string_view>
 #include <string>
+#include <vector>
 
 class VetClinic
 {
 private:
-    Animal* animal;
-    int animalSize;
-
-    Vet* vet;
-    int vetSize;
-
-    Appeals* appeals;
-    int appealsSize;
+    std::vector<Animal> animal;
+    std::vector<Vet> vet;
+    std::vector<Appeals> appeals;
 public:
     VetClinic();
-    ~VetClinic();
 
     VetClinic(const VetClinic&) = delete;
     VetClinic& operator=(const VetClinic&) = delete;
 
-    void setAnimal(const Animal& animal);
-    void setVet(const Vet& vet);
-    bool setAppeals(const std::string& vetSpeciality, const std::string& nameAnimal, const std::string& date, const std::string& diagnosis, const std::string& treatment);
+    void setAnimal(const Animal& animalTemp);
+    void setVet(const Vet& vetTemp);
+    bool setAppeals(std::string_view vetSpeciality, std::string_view nameAnimal, std::string_view date, std::string_view diagnosis, std::string_view treatment);
 
-    bool appointmentCheck(const std::string& vetSpeciality, const std::string& viewAnimal);
+    bool appointmentCheck(std::string_view vetSpeciality, std::string_view viewAnimal) const;
 
     void infoAnimal() const;
     void infoVet() const;
